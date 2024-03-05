c887650f811f18b00fc0b22feb19601030755fe6
Nugroho Priyo Utomo <nugrohopriyoutomo@students.amikom.ac.id>
2024-03-05 11:45:01 +0700

    menambahkan file calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,20 @@
+class Calculator
+{
+	static void Main(string[] args)
+	{
+		int a = 10;
+		int b = 6;
+	
+	}
+	
+	static int Penambahan(int a, int b)
+	{
+		
+	}
+
+	static int Pengurangan(int a, int b)
+	{
+		
+	}
+	
+}
